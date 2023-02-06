b5be0f69356e469f6cca7b25fff146a0518eb8e3
120072370 <120072370@qq.com>
2023-02-06 16:06:52 +0800

    no message

--- iphone.go
+++ iphone.go
@@ -4,12 +4,10 @@ func main() {
 
 	/**
 	2代笔
-	10.2   iPad pro (第九代)  2021款 10.2英寸  iPad pro (第九代)
-	11     iPad pro (第四代)  2022款 11英寸 iPad pro  (第四代)
 	12.9   iPad pro (第3代和第4代和第5代和第6代) 2021款 12.9英寸 iPad pro  (第五代) 2022款 12.9英寸 iPad pro  (第六代)
-	11     iPad pro (第1代和第2代和第3代) 2021款 11英寸 iPad pro  (第三代)
-	iPad Air        (第四代和第五代) 2020款 10.9英寸 iPad Air  (第四代) 2022款 10.9英寸 iPad Air  (第五代)
-	iPad mini       (第六代)  2021款 10.9英寸 iPad mini  (第六代)
+	11     iPad pro (第1代和第2代和第3代和第4代) 2021款 11英寸 iPad pro  (第三代) 128 m1 5599   2022款 11英寸 iPad pro  (第四代)  128 m2 6398
+	iPad Air        (第四代和第五代) 2020款 10.9英寸 iPad Air  (第四代) 2022款 10.9英寸 iPad Air  (第五代) m1 4398
+	iPad mini       (第六代)  2021款 10.9英寸 iPad mini  (第六代) 3999 a15
 
 	 */
 }
